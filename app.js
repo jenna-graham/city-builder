073d312658b92b103ebca30f354cc1ba8b08e7a1
jenna-graham <jclayto2@gmail.com>
2022-04-28 16:38:32 -0700

    grabbed dom elements by id, set event listener for 'climate' added display count function for climate

--- app.js
+++ app.js
@@ -1,10 +1,32 @@
 // import functions and grab DOM elements
+const climateSelect = document.getElementById('climate-select');
+const climateDiv = document.getElementById('climate');
+const countEl = document.getElementById('count');
+
+
+
+
+
 
 
 
 // let state
+let climateCount = 0;
 
 // set event listeners 
+climateSelect.addEventListener('change', () => {
+    console.log('changing climate', climateSelect.value);
+    climateCount++;
+    climateDiv.style.background = `url('./assets/${climateSelect.value}.jpg')`;
+    displayStats();
+
+});
   // get user input
   // use user input to update state 
   // update DOM to reflect the new state
+
+function displayStats() {
+    countEl.textContent = `You have changed the climate ${climateCount} times.`;
+
+
+}
