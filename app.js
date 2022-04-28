74f83f0eebfec2c8e0dc6c49666ab5201cf382ac
jenna-graham <jclayto2@gmail.com>
2022-04-28 16:49:06 -0700

    added dom elements, event listeners and display stats function to the 'known for' drop down

--- app.js
+++ app.js
@@ -1,17 +1,12 @@
 // import functions and grab DOM elements
 const climateSelect = document.getElementById('climate-select');
 const climateDiv = document.getElementById('climate');
+const knownForSelect = document.getElementById('known-for-select');
+const knownForDiv = document.getElementById('known-for');
 const countEl = document.getElementById('count');
-
-
-
-
-
-
-
-
 // let state
 let climateCount = 0;
+let knownForCount = 0;
 
 // set event listeners 
 climateSelect.addEventListener('change', () => {
@@ -21,12 +16,15 @@ climateSelect.addEventListener('change', () => {
     displayStats();
 
 });
-  // get user input
-  // use user input to update state 
-  // update DOM to reflect the new state
+knownForSelect.addEventListener('change', () => {
+    console.log('changing known for', knownForSelect.value);
+    knownForCount++;
+    knownForDiv.style.background = `url('./assets/${climateSelect.value}.jpg')`;
+    displayStats();
+
+ });
 
 function displayStats() {
-    countEl.textContent = `You have changed the climate ${climateCount} times.`;
-
+    countEl.textContent = `You have changed the climate ${climateCount} and what the city is known for ${knownForCount} times.`;
 
 }
